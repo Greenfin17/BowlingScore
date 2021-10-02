f70066c8d828b6afca91a367bd9cfb92dfa75e26
John Maple <jmaple@bellsouth.net>
2021-10-02 12:21:15 -0500

    adding tests for string input

--- Bowling.Test/BowlingScoreTest.cs
+++ Bowling.Test/BowlingScoreTest.cs
@@ -23,8 +23,16 @@ namespace Bowling.Test
         [Theory]
         
         [InlineData(9,1,10,0,8,1,8,2,6,3,7,2,7,3,7,2,9,1,9,1,8,145)]
+        // input with errors - frames add up to over 10
+        [InlineData(9,5,10,2,8,1,8,2,6,3,7,2,7,3,7,2,9,2,9,3,8,145)]
+        [InlineData(9,1,10,0,8,1,8,2,6,3,7,3,7,3,7,2,9,2,9,3,8,153)]
+        // input with errors - frames add up to over 10
+        [InlineData(9,5,10,2,8,1,8,2,6,3,7,5,7,3,7,2,9,2,9,3,8,153)]
         [InlineData(6,4,4,4,8,1,8,2,6,3,7,3,9,1,9,0,9,1,10,10,8,151)]
         [InlineData(5,3,2,3,4,5,10,0,9,1,8,1,4,5,10,0,8,1,10,6,4,126)]
+        [InlineData(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)]
+        [InlineData(10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,300)]
+        [InlineData(10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,10,10,300)]
         public void bowling_score_theory(int a1, int a2, int b1, int b2, int c1, int c2, int d1, int d2, int e1, int e2,
                                          int f1, int f2, int g1, int g2, int h1, int h2, int i1, int i2, int j1, int j2, int j3, int expectedResult)
         
@@ -38,8 +46,69 @@ namespace Bowling.Test
             var actualResult = game.GetScore();
 
             // Assert
-            Equals(expectedResult, actualResult);
+            Assert.Equal(expectedResult, actualResult);
 
         }
+        
+        [Theory]
+        [InlineData(9,1,10,0,8,1,8,2,58)]
+        [InlineData(7,3,9,1,8,1,10,0,56)]
+        public void bowling_score_theory_4_frames_partial_game(int a1, int a2, int b1, int b2, int c1, int c2, int d1, int d2, int expectedResult)
+        {
+
+            Frame[] frameList = { new Frame(a1, a2), new Frame(b1, b2), new Frame(c1, c2), new Frame(d1, d2) };
+            var game = new BowlingGame(frameList);
+
+            // Act
+            var Result1 = game.GetScore();
+            var actualResult = game.GetScore();
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+        [Theory]
+        [InlineData("9","1","10","0","8","1","8","2","6","3","7","2","7","3","7","2","9","1","9","1","8",145)]
+        [InlineData("9","/","X","0","8","1","8","/","6","3","7","2","7","/","7","2","9","/","9","/","8",145)]
+        [InlineData("6","4","4","4","8","1","8","2","6","3","7","3","9","1","9","0","9","1","10","10","8",151)]
+        [InlineData("6","/","4","4","8","1","8","/","6","3","7","/","9","/","9","0","9","/","X","X","8",151)]
+        public void bowling_score_theory_string_input(string a1, string a2, string b1, string b2, string c1, string c2, string d1, string d2, string e1, string e2,
+                                         string f1, string f2, string g1, string g2, string h1, string h2, string i1, string i2, string j1, string j2, string j3, int expectedResult)
+        
+        {
+            Frame[] frameList = { new Frame(a1, a2), new Frame(b1, b2), new Frame(c1, c2), new Frame(d1, d2), new Frame(e1, e2),
+                                  new Frame(f1, f2), new Frame(g1, g2), new Frame(h1, h2), new Frame(i1, i2), new Frame(j1, j2, j3) };
+            var game = new BowlingGame(frameList);
+
+            // Act
+            var Result1 = game.GetScore();
+            var actualResult = game.GetScore();
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+       
+        [Theory]
+        [InlineData("9","1","10","0","8","1","8","2",58)]
+        [InlineData("9","/","X","0","8","1","8","/",58)]
+        [InlineData("7","3","9","1","8","1","10","0",56)]
+        // with invalid input
+        [InlineData("7","/","9","/","8","1","X","2",56)]
+        // with invalid input
+        [InlineData("7","/","9","/","8","1","10","X",56)]
+        public void bowling_score_theory_4_frames_partial_game_with_strings(string a1, string a2, string b1, string b2, string c1, string c2, string d1, string d2, int expectedResult)
+        {
+
+            Frame[] frameList = { new Frame(a1, a2), new Frame(b1, b2), new Frame(c1, c2), new Frame(d1, d2) };
+            var game = new BowlingGame(frameList);
+
+            // Act
+            var Result1 = game.GetScore();
+            var actualResult = game.GetScore();
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+        
     }
 }
--- Bowling/BowlingGame.cs
+++ Bowling/BowlingGame.cs
@@ -14,12 +14,20 @@ namespace Bowling
         {
             Frames = new Frame[] { };
         }
-        public BowlingGame(Frame[] frames)
+        public BowlingGame(Frame[] inputFrames)
         {
-            Frames = frames;
+            Frames = new Frame[10];
+            for ( int i = 0; i < inputFrames.Length; i++)
+            {
+                Frames[i] = inputFrames[i];
+            }
+            for ( int i = inputFrames.Length; i < 10; i++)
+            {
+                Frames[i] = new Frame();
+            }
+
         }
 
-        private bool _isProcessed = false;
         public int GetScore()
         {
             int returnVal = 0;
@@ -41,7 +49,7 @@ namespace Bowling
                     {
                         Frames[i].Score += Frames[i + 1].R1 + Frames[i + 2].R1;
                     }
-                    // Next frame spare
+                    // Next frame spare or open
                     else if (Frames[i + 1].R1 < 10)
                     {
                         Frames[i].Score += Frames[i + 1].R1 + Frames[i + 1].R2;
@@ -73,7 +81,7 @@ namespace Bowling
         {
             int returnVal = -1;
             int sum = 0;
-            if (Frames.Length == 10 && _isProcessed)
+            if (Frames.Length == 10 )
             {
                 for (int i = 0; i < 10; i++)
                 {
@@ -94,9 +102,7 @@ namespace Bowling
         internal int R2;
         internal int R3;
         internal int Score;
-
-
-
+        internal bool isFinalFrame = false;
 
         public Frame()
         {
@@ -113,14 +119,6 @@ namespace Bowling
             R3 = 0;
             Score = 0;
         }
-        public Frame(string roll1, string roll2)
-        {
-            SetRollOne(roll1);
-            SetRollTwo(roll2);
-            R3 = 0;
-            Score = 0;
-        }
-
         public Frame(int roll1, int roll2)
         {
             SetRollOne(roll1);
@@ -130,14 +128,23 @@ namespace Bowling
 
         public Frame(int roll1, int roll2, int roll3)
         {
+            isFinalFrame = true;
             SetRollOne(roll1);
             SetRollTwo(roll2);
             SetRollThree(roll3);
             Score = 0;
         }
+        public Frame(string roll1, string roll2)
+        {
+            SetRollOne(roll1);
+            SetRollTwo(roll2);
+            R3 = 0;
+            Score = 0;
+        }
 
         public Frame(string roll1, string roll2, string roll3)
         {
+            isFinalFrame = true;
             SetRollOne(roll1);
             SetRollTwo(roll2);
             SetRollThree(roll3);
@@ -151,64 +158,75 @@ namespace Bowling
             if (int.TryParse(roll, out value) && value >= 0 && value <= 10)
             {
                 R1 = value;
-                if (R1 == 10)
-                {
-                    R2 = 0;
-                    R3 = 0;
-                }
             }
-            else if (roll.Equals("X"))
+            else if (roll.ToUpper().Equals("X"))
             {
                 R1 = 10;
-                R2 = 0;
-                R3 = 0;
             }
         }
         public void SetRollOne(int roll)
         {
-            if (roll >= 0 && roll < 10)
+            if (roll >= 0 && roll <= 10)
             {
                 R1 = roll;
-            }
-            else if (roll == 10)
-            {
-                R1 = roll;
-                R2 = 0;
-                R3 = 0;
             }
         }
         public void SetRollTwo(string roll)
         {
             int value;
             roll.Trim();
-            if (R1 < 10 && int.TryParse(roll, out value) && value >= 0 && value <= 10 && (R1 + value) <= 10)
-            {
-                R2 = value;
+            if (int.TryParse(roll, out value) && value >= 0 && value <= 10) {
+                if (!isFinalFrame)
+                {
+                    if (R1 + value <= 10)
+                    {
+                        R2 = value;
+                    }
+                    else R2 = 10 - R1;
+                }
+                else
+                {
+                    if (R1 == 10 || (R1 + value <= 10))
+                    {
+                        R2 = value;
+                    }
+                    else R2 = 10 - value;
+                }
+
             }
-            else if (R1 == 10)
-            {
-                R2 = 0;
-                R3 = 0;
-            }
-            else if (roll.Equals("\\"))
+            else if (roll.Equals("/"))
             {
                 R2 = 10 - R1;
-                R3 = 0;
+            }
+            else if (isFinalFrame && roll.ToUpper().Equals("X"))
+            {
+                // set roll 2 to ten only if roll 1 was a strike
+                if (R1 == 10) R2 = 10;
+                else R2 = 10 - R1;
             }
         }
         public void SetRollTwo(int roll)
         {
-            if (R1 < 10 && roll >= 0 && roll <= 10 && (R1 + roll) <= 10)
-            {
-                R2 = roll;
-            }
-        }
-
-        public void SetRollThree(int roll)
-        {
             if (roll >= 0 && roll <= 10)
             {
-                R3 = roll;
+                if (!isFinalFrame)
+                {
+                    if (R1 + roll <= 10)
+                    {
+                        R2 = roll;
+                    }
+                    // error handling
+                    else R2 = 10 - R1;
+                }
+                else
+                {
+                    if (R1 == 10 || (R1 + roll <= 10))
+                    {
+                        R2 = roll;
+                    }
+                    // roll 1 + roll2 add up to more than ten, error handling
+                    else R2 = 10 - R1;
+                }
             }
         }
 
@@ -218,6 +236,19 @@ namespace Bowling
             if (Int32.TryParse(roll, out value) && value >= 0 && value <= 10)
             {
                 R3 = value;
+            }
+            else if (roll.Equals("/")){
+                R3 = 10 - R2;
+            }
+            else if (roll.ToUpper().Equals("X")){
+                R3 = 10;
+            }
+        }
+        public void SetRollThree(int roll)
+        {
+            if (roll >= 0 && roll <= 10)
+            {
+                R3 = roll;
             }
         }
         public int GetScore()
